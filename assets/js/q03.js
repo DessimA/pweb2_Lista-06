8714c1af9a5f70c2a263f6cb6ba614a49fb11a37
José Anderson da Silva Costa <60760405+DessimA@users.noreply.github.com>
2022-11-10 23:42:26 -0300

    Update q03.js

---
+++ assets/js/q03.js
@@ -0,0 +1,7 @@
+let ordemDecrescente = [34, 6, 73, -1, 43, 12, -9]
+
+document.write("Entrada: " + ordemDecrescente);
+
+ordemDecrescente.sort((a, b) => b - a);
+
+document.write("</br>Saída: " + ordemDecrescente);
