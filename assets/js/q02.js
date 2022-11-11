c20964bfe6282e0277bfb2e63f4eb66a2aab9976
José Anderson da Silva Costa <60760405+DessimA@users.noreply.github.com>
2022-11-10 23:20:05 -0300

    Update q02.js

--- assets/js/q02.js
+++ assets/js/q02.js
@@ -1,11 +1,14 @@
 function somaDosPositivos(n1, n2, n3 ,n4, n5){
     let arr = [n1,n2,n3,n4,n5];
+    let soma = 0;    
 
-    let arr2 = arr.filter( number => number >= 0);
-    return arr2;
+    document.write("Entrada: "+ arr);
+
     for(i=0; i < arr.length; i++){
-        let soma += arr2[i];
+       if (arr[i] > 0)
+        soma += arr[i];
     }
+    return soma;
 }
 
-console.log(somaDosPositivos(-2, 0, 3, -9, 7));
+document.write("</br>Soma dos positivos: " + somaDosPositivos(-2, 0, 3, 9, 7));
