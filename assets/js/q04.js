f9132a28de9d15138402f88e9651748ab9487422
José Anderson da Silva Costa <60760405+DessimA@users.noreply.github.com>
2022-11-10 23:54:00 -0300

    Update q04.js

--- assets/js/q04.js
+++ assets/js/q04.js
@@ -1,8 +1,10 @@
 let mediaDoArray = [10,5,5,5,3,9];
 let soma = 0;
 
+document.write("Entrada: " + mediaDoArray);
+
 for (i = 0; i < mediaDoArray.length; i++){
     soma += mediaDoArray[i];
 }
 
-document.write("A média é: " + (soma/mediaDoArray.length).toFixed(2));
+document.write("</br>A média é: " + (soma/mediaDoArray.length).toFixed(2));
